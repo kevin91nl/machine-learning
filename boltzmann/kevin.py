42736fb79fbd8d14f42f32a9aa7bee93add0d9ea
Kevin Jacobs <kevin91nl@gmail.com>
2016-12-20 15:52:19 +0100

    Python conversion

---
+++ boltzmann/kevin.py
@@ -0,0 +1,63 @@
+import random
+
+import scipy.io
+import numpy as np
+from scipy.stats import bernoulli
+
+
+class DataLoader:
+    def __init__(self, file):
+        self.data = scipy.io.loadmat(file, squeeze_me=True, struct_as_record=False)['mnist']
+        self.train_images = self.reshape_images(self.data.train_images)
+        self.test_images = self.reshape_images(self.data.test_images)
+        self.train_labels = self.data.train_labels
+        self.test_labels = self.data.test_labels
+
+    def reshape_images(self, data):
+        reshaped = data.reshape(data.shape[0] * data.shape[1], data.shape[2])
+        swapped_axes = np.swapaxes(reshaped, 0, 1)
+        return swapped_axes
+
+
+# data = DataLoader('mnistAll.mat')
+# print(data.train_images.shape)
+
+# Define the number of neurons
+num_neurons = 784
+
+# Initialize the weights and biases
+w = 2 * np.random.normal(0, 1, (num_neurons, num_neurons))
+theta = np.random.normal(0, 1, (num_neurons, 1)).ravel()
+m = np.random.normal(0, 1, (num_neurons, 1)).ravel()
+
+
+def approximate_m(m, w, theta, num_iterations=10):
+    for _ in range(num_iterations):
+        m = np.tanh(np.dot(w, m) - theta)
+    return m
+
+
+def calculate_log_qi(s, m, epsilon=0.00001):
+    return s.shape[0] * np.log(1 / 2) + np.log(s * m + 1 + epsilon)
+
+
+def calculate_energy(s, w, theta):
+    energy = np.dot(theta, s)
+    for i in range(s.shape[0]):
+        for j in range(s.shape[0]):
+            energy += 0.5 * w[i, j] * s[i] * s[j]
+    return energy
+
+
+m = approximate_m(m, w, theta)
+s = np.ones((num_neurons,))
+qi = np.exp(calculate_log_qi(s, m))
+num_samples = 10
+samples = [bernoulli.rvs(0.5, size=(num_neurons,)) * 2 - 1 for _ in range(num_samples)]
+for s in samples:
+
+log_qis = [calculate_log_qi(s, m) for s in samples]
+print(log_qis[0])
+s_q = -1 * np.sum([np.exp(log_qi) * log_qi for log_qi in log_qis])
+expected_energy = [calculate_energy(s, w, theta) * np.exp(log_qi) for log_qi, s in zip(log_qis, samples)]
+print(expected_energy)
