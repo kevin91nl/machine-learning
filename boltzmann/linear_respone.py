5caf40fbee81755a617766533529877cae31d2c8
Kevin Jacobs <kevin91nl@gmail.com>
2016-12-20 19:53:08 +0100

    Fix linear response.

--- boltzmann/linear_respone.py
+++ boltzmann/linear_respone.py
@@ -1,4 +1,6 @@
+import numpy as np
 from numpy import linalg
+
 
 def compute_stats(m, w):
     """
@@ -11,5 +13,5 @@ def compute_stats(m, w):
         A[i][i] = 1 / (1 - m[i] * m[i])
     A_inv = linalg.inv(A)
     s_i_s_j = np.dot(m.reshape(len(m), 1), m.reshape(1, len(m))) + A_inv
-    
+
     return s_i, s_i_s_j
