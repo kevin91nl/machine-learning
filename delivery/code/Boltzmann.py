7df93894c0deea68eb09ecabe66bcbebd9459848
Kevin Jacobs <kevin91nl@gmail.com>
2017-01-31 17:18:53 +0100

    Codes

---
+++ delivery/code/Boltzmann.py
@@ -0,0 +1,230 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rc
+import seaborn as sns
+import scipy.io
+import random
+rc('text', usetex=True)
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+class DataLoader:
+    """
+    Class for loading the MatLab data.
+    """
+
+    def __init__(self, file):
+        """
+        Load the data from the given file.
+
+        :param file: Path to file to load the data from
+                (without the .mat extension).
+        :param flip_rate: Rate for flipping bits randomly,
+            0 means no flipping and 1 means all flipped and the
+            maximum entropy is obtained for flip_rate = 0.5.
+        """
+        self.data = scipy.io.loadmat(file, squeeze_me=True,
+                                     struct_as_record=False)['mnist']
+        self.train_images = self.data.train_images
+        self.test_images = self.data.test_images
+        self.train_labels = self.data.train_labels
+        self.test_labels = self.data.test_labels
+
+    def load_images(self, flip_rate=0):
+        self.train_images = self.transform_images(self.train_images)
+        self.test_images = self.transform_images(self.test_images)
+        if flip_rate > 0:
+            noise = np.random.binomial(
+                1, flip_rate, self.train_images.shape) * -2 + 1
+            self.train_images = np.multiply(self.train_images, noise)
+
+    def transform_images(self, data):
+        """
+        Convert a (m x n x p) array to a (p x m x n) array and
+        apply some additional transformations.
+
+        :param data: Data to transform.
+        :return: Transformed data.
+        """
+        reshaped = data.reshape(data.shape[0] * data.shape[1],
+                                data.shape[2])
+        swapped_axes = np.swapaxes(reshaped, 0, 1)
+        return (swapped_axes > 122) * 2 - 1
+    
+def calculate_normalizing_constant(samples, w, theta):
+    return np.sum(np.exp(-calculate_energy(samples, w, theta)))
+
+def calculate_probabilities(samples, w, theta, normalizing_constant):
+    return np.exp(-calculate_energy(samples, w, theta)) / \
+           normalizing_constant
+
+def calculate_energy(samples, w, theta):
+    f = np.dot(samples, np.dot(w, samples.T))
+    # Also allow samples consisting of one sample (an array,
+    # so f.ndim == 1)
+    # Therefore, only take the diagonal in the two dimensional case
+    if f.ndim == 2:
+        f = np.diagonal(f)
+    return np.squeeze(np.asarray(-0.5 * f - np.dot(
+        theta.T, samples.T)))
+
+def generate_samples(w, theta, num_burn_in=50, num_samples=500,
+                     show_transition_probabilities=False):
+    num_neurons = w.shape[0]
+    
+    # Initialize a random sample
+    s = np.random.binomial(1, 0.5, (num_neurons,)) * 2 - 1
+    
+    # Initialize the matrix of generated samples
+    X = np.empty((0, num_neurons))
+    
+    # Iterate (first generate some samples during the burn-in
+    # period and then gather the samples)
+    for iteration in range(num_samples):
+        for burn_in in range(num_burn_in + 1):
+            # Store the original value of s
+            s_original = s
+            # Calculate the flip probabilities
+            p_flip = 0.5 * (1 + np.tanh(np.multiply(
+                -s, np.dot(w, s) + theta)))
+            # Calculate transition probabilities
+            p_transition = p_flip / float(num_neurons)
+            p_stay = 1 - np.sum(p_transition)
+            # Flip according to the probability distribution of flipping
+            if random.random() <= 1 - p_stay:
+                # Pick a random neuron
+                neuron = random.randint(1, num_neurons) - 1
+                if random.random() <= p_flip[neuron]:
+                    s[neuron] *= -1
+            # Add the state if the sample is not generated
+            # during the burn in period
+            if burn_in >= num_burn_in:
+                if show_transition_probabilities:
+                    print('Transition probabilities for ',
+                          s_original,':', p_transition,
+                          ' (stay probability: ', p_stay, ')')
+                X = np.vstack([X, s])
+    return X
+
+def calculate_clamped_statistics(X):
+    """
+    Calculate <x_i>_c and <x_i x_j>_c given X.
+    """
+    num_datapoints = X.shape[0]
+    return np.sum(X, axis=0) / num_datapoints, \
+           np.dot(X.T, X) / num_datapoints
+
+# Function for training using Gibbs Sampling
+def training_bm(num_burnin, num_samples):
+    num_neurons = 10
+    learning_rates = [0.05, 0.05]
+    w = np.random.normal(0, 1, (num_neurons, num_neurons))
+    w = np.tril(w) + np.tril(w, -1).T
+    np.fill_diagonal(w, 0)
+    theta = np.random.normal(0, 1, (num_neurons,))
+
+    X_c = np.random.binomial(1, 0.5, (50, num_neurons)) * 2 - 1
+    s1_c, s2_c = calculate_clamped_statistics(X_c)
+
+    q = []
+    for _ in range(150):
+        X = generate_samples(w, theta, num_burnin, num_samples)
+        Z = calculate_normalizing_constant(X, w, theta)
+        p = calculate_probabilities(X, w, theta, Z)
+
+        p_repeat = np.tile(p, (num_neurons, 1)).T
+        Q = np.multiply(p_repeat, X)
+        s2 = np.dot(X.T, Q)
+        s1 = np.dot(p, X)
+
+        dLdw = s2_c - s2
+        dLdtheta = s1_c - s1
+        np.fill_diagonal(dLdw, 0)
+
+        delta_w = learning_rates[0] * dLdw
+        delta_theta = learning_rates[1] * \
+                      np.squeeze(np.asarray(dLdtheta))
+
+        w += delta_w
+        theta += delta_theta
+
+        q.append([np.sum(np.abs(dLdw)), np.sum(np.abs(dLdtheta))])
+    return np.matrix(q)
+
+
+###########################################################
+##### Absolute change in weight with varying samples ######
+###########################################################
+
+q = training_bm(0, 40)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+fit = np.polyfit(np.arange(150), q[:, 0], 1, full=True)
+ax[0].plot(q[:, 0], label='Absolute change in weights')
+ax[0].plot(np.arange(150), fit[0][0] * np.arange(150) + fit[0][1],
+           '--r', label='Linear fit')
+ax[0].legend()
+ax[0].set_xlabel('Iteration')
+ax[0].set_ylabel('Absolute change in weights')
+ax[0].set_title('$0$ burn-in samples, $40$ used samples')
+
+q = training_bm(400, 100)
+fit = np.polyfit(np.arange(150), q[:, 0], 1, full=True)
+ax[1].plot(q[:, 0], label='Absolute change in weights')
+ax[1].plot(np.arange(150), fit[0][0] * np.arange(150) + fit[0][1],
+           '--r', label='Linear fit')
+ax[1].legend()
+ax[1].set_xlabel('Iteration')
+ax[1].set_ylabel('Absolute change in weights')
+ax[1].set_title('$400$ burn-in samples, $100$ used samples')
+
+
+
+#################################
+### Mean Field Approximation ####
+#################################
+
+num_neurons = 28 * 28
+learning_rate = 0.01
+w = np.random.normal(0, 1, (num_neurons, num_neurons))
+theta = np.random.normal(0, 1, (num_neurons,))
+np.fill_diagonal(w, 1)
+
+def calculate_probabilities(samples, w, theta,
+                            normalizing_constant):
+    return np.exp(-calculate_energy(
+        samples, w, theta)) / normalizing_constant
+
+def calculate_energy(samples, w, theta):
+    f = np.dot(samples, np.dot(w, samples.T))
+    # Also allow samples consisting of one sample
+    # (an array, so f.ndim == 1)
+    # Therefore, only take the diagonal in the two dimensional case
+    if f.ndim == 2:
+        f = np.diagonal(f)
+    return np.squeeze(np.asarray(-0.5 * f - np.dot(
+        theta.T, samples.T)))
+
+
+def train_classifiers(samples, labels):
+    w = np.zeros((10, 28 * 28, 28 * 28))
+    theta = np.zeros((10, 28 * 28))
+    Z = np.zeros(10)
+    for digit in range(0, 10):
+        training_samples = samples[labels == digit]
+        s_mean_clamped = np.squeeze(np.asarray(np.mean(
+            training_samples, 0)))
+        s_cov_clamped = np.cov(training_samples.T)
+        m = s_mean_clamped
+        C = s_cov_clamped - np.dot(np.asmatrix(
+            s_mean_clamped).T, np.asmatrix(s_mean_clamped))
+        delta = np.zeros(s_cov_clamped.shape)
+        np.fill_diagonal(delta, 1. / (1. - np.multiply(m, m)))
+        w[digit, :, :] = delta - np.linalg.inv(C)
+        theta[digit, :] = np.arctanh(m) - np.dot(w[digit, :, :], m)
+        F = -0.5 * np.dot(np.dot(m, w[digit, :, :]), m) - \
+            np.dot(theta[digit, :], m) + 0.5 * np.dot(
+            1 + m, np.log(0.5 * (1 + m))) + 0.5 * np.dot(
+            1 - m, np.log(0.5 * (1 - m)))
+        Z[digit] = np.exp(-F)
+    return w, theta, Z
+
+w, theta, Z = train_classifiers(data.train_images, data.train_labels)
