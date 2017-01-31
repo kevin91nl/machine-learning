08cd3dfb8efa927fcfe850efde5132f73115b1f7
Kevin Jacobs <kevin91nl@gmail.com>
2017-01-31 12:11:28 +0100

    Updates

--- simulated-annealing/sa/optimizer.m
+++ simulated-annealing/sa/optimizer.m
@@ -120,9 +120,9 @@ case 'sa'
 	T1=1000; % length markov chain at fixed temperature
 	factor=1.05 ; % increment of beta at each new chain
     
-    beta_init = 0.01;
-    factor = 2;
-    T1=100;
+    %beta_init = 2;
+    %factor = 2;
+    T1=1000;
 
 	beta=beta_init;
 	E_bar(1)=1;
